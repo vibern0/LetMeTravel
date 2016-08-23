04f3f8ab8a550688be98918cf1adbda9c75ca70d
bernardovieira <iamthekyt@gmail.com>
2016-08-23 13:28:43 +0100

    Schedule
    - Load Schedule from station to station by selecting week day

--- website/index.php
+++ website/index.php
@@ -16,6 +16,10 @@
                     {
                         t.innerHTML = xmlhttp.responseText;
                         document.getElementById("travelPrice").innerHTML = '0€';
+                        document.getElementById("week_day").selectedIndex = 0;
+                        //clear schedule table
+                        document.getElementById("schedule_table").innerHTML =
+                            "<tr><th>Leave time</th><th>Travel time</th><th>Arrive time</th></tr>";
                     }
                 };
                 xmlhttp.open("GET", "scripts/loadDestStations.php?f=" + f.options[f.selectedIndex].value, true);
@@ -32,7 +36,6 @@
                     if (xmlhttp.readyState == 4 && xmlhttp.status == 200)
                     {
                         document.getElementById("travelPrice").innerHTML = xmlhttp.responseText + '€';
-                        loadTravelSchedule();
                     }
                 };
                 xmlhttp.open("GET", "scripts/getTravelPrice.php?f=" +
@@ -41,7 +44,22 @@
             }
             function loadTravelSchedule()
             {
+                var t = document.getElementById("toStations");
+                var f = document.getElementById("fromStation");
+                var w = document.getElementById("week_day");
+                var xmlhttp = new XMLHttpRequest();
 
+                xmlhttp.onreadystatechange = function()
+                {
+                    if (xmlhttp.readyState == 4 && xmlhttp.status == 200)
+                    {
+                        document.getElementById("schedule_table").innerHTML += xmlhttp.responseText;
+                    }
+                };
+                xmlhttp.open("GET", "scripts/getTravelScheduleInfo.php?f=" +
+                    f.options[f.selectedIndex].value + "&t=" + t.options[t.selectedIndex].value +
+                    "&w=" + w.options[w.selectedIndex].value, true);
+                xmlhttp.send();
             }
         </script>
     </head>
@@ -60,14 +78,24 @@
         <select id="toStations" onchange="getTravelPrice()">
             <option value="null">Select</option>
         </select>
-        <span id="travelPrice">0€</price>
+        <span id="travelPrice">0€</span>
+        Week day
+        <select id="week_day" onchange="loadTravelSchedule()">
+            <option value="null">Select</option>
+            <option value="1">Monday</option>
+            <option value="2">Tuesday</option>
+            <option value="3">Wednesday</option>
+            <option value="4">Thursday</option>
+            <option value="5">Friday</option>
+            <option value="6">Saturday</option>
+            <option value="7">Sunday</option>
+        </select>
         Schedule
         <table id="schedule_table">
             <tr>
-                <th>Leave</th>
-                <th>Arrive</th>
+                <th>Leave time</th>
                 <th>Travel time</th>
-                <th>Travel stops</th>
+                <th>Arrive time</th>
             </tr>
         </table>
     </body>
